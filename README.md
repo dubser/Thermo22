62b4988f0ee2a005c9090bf87bb68c361f2ff459
dubser <dubser123@hotmail.com>
2025-09-07 07:23:47 -0400

    Ajout de librairie pubsubclient et service mDns ajout

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Connexion web élémentaire utilisant
 le protocole de connection mdns.
 Les ports sécuritaires GPIO 13, 14, 16, 17, 18, 19, 21, 22, 23, 25, 26, 27, 32, 33
 Ajout d'une structure pour invisibiliser les passwd sur GitHub et GPIO21 => GPIO23
+07 sept 2025 On est a Flash: 57.5% (used 753185 bytes from 1310720 sur HARDWARE: ESP32 240MHz, 320KB RAM, 4MB Flash
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include "DHT.h"
 #include "secret.h"
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <PubSubClient.h>
 
 // Définition du GPIO utilisé
 //#define DHTPIN 21       // Broche GPIO21
@@ -14,13 +17,39 @@ void setup() {
   Serial.begin(9600);
   Serial.println("DHT22 test sur ESP32 WROOM-32");
 
+  // Définir le hostname
+  //WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE); // obligatoire pour certains firmwares
+  //WiFi.setHostname("MonHostName"); 
+
+// Connexion au WiFi
+  WiFi.begin(wifiId, pasw);
+  Serial.print("Connexion au WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connecté !");
+  Serial.println(WiFi.localIP());
+  // Adresse MAC en mode station (STA)
+  Serial.print("MAC (STA) : ");
+  Serial.println(WiFi.macAddress());
+
   dht.begin();
-  Serial.println(WIFI_PASSWORD);
+
+    // Démarrage du service mDNS
+  if (!MDNS.begin("Thermodht")) {   // => Nom d’hôte : esp32wroom.local
+    Serial.println("Erreur : mDNS n’a pas pu démarrer");
+    while (1) {
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS actif : thermodht.local");
+
 }
 
 void loop() {
   // Délai minimum recommandé entre 2 lectures (~2s pour DHT22)
-  delay(2000);
+  delay(4000);
 
   float h = dht.readHumidity();
   float t = dht.readTemperature();        // °C
